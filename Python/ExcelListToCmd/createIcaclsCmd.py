90932cb11192eaf6a76304ede13c92edde2ce279
takakichi <antlers.takapon.kun@gmail.com>
2018-11-08 00:56:13 +0900

    Update Python/ExcelListToCmd/createIcaclsCmd.py

--- Python/ExcelListToCmd/createIcaclsCmd.py
+++ Python/ExcelListToCmd/createIcaclsCmd.py
@@ -4,6 +4,7 @@ from jinja2 import Template
 # icacls [ファイル名] /grant [ユーザー名]:[perm]
 cmd_grant_file = "icacls {filename} "
 cmd_deny_file = "icacls {} {} {}"
+cmd_grant_dir = ""
 cmd_deny_dir = ""
 
 def createIcaclsCmd():
